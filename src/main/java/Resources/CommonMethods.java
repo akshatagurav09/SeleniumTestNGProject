ddcdfd1804581b225b8d846e3b0528bc9b38b0c7
akshatagurav09 <akshatagurav09@gmail.com>
2023-09-17 21:18:10 +0530

    Added explicit wait code

--- src/main/java/Resources/CommonMethods.java
+++ src/main/java/Resources/CommonMethods.java
@@ -1,8 +1,13 @@
 package Resources;
 
+import java.time.Duration;
 import java.util.List;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.asserts.SoftAssert;
 
 public class CommonMethods {
@@ -31,4 +36,11 @@ public class CommonMethods {
 			}
 		}
 	}
+	
+	public static void expWait(WebDriver driver, WebElement xpath, int timeout)
+	{
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(timeout));
+		wait.until(ExpectedConditions.elementToBeClickable(xpath));
+	}
+	
 }
